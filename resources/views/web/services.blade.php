e189fca1da40043098f01e875f961f06f265a6e7
beetlejuice96 <dilanbernbae043@gmail.com>
2020-12-06 11:34:08 -0300

    services terminado(creo)

---
+++ resources/views/web/services.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.layout')
+@section('location')
+    Services
+@endsection
+
+@section('content')
+    <article>
+        <h2 >¿Por qué es tan importante hacer el service oficial preconizado por Renault?</h2>
+        <ul >
+            <li>Porque te damos seguridad por contar con profesionales especializados.</li>
+            <li>Porque tenemos el equipamiento con la tecnología necesaria para tu vehículo.</li>
+            <li>Porque tenemos un plan de mantenimiento personalizado para tu modelo.</li>
+            <li>Porque nuestros repuestos son originales para brindarte la confianza y durabilidad que estás buscando.</li>
+            <li>Porque cumplir con los planes de mantenimiento permite mantener la garantía de tu vehículo.</li>
+            <li>Porque nuestros precios son transparentes.</li>
+            <li>Porque tenemos la red de servicio más amplia del país.</li>
+        </ul>
+        <img src="" alt="imagen1">
+    </article>
+
+    <hr>
+
+    <article>
+        <h2 > Te proponemos realizar las siguientes operaciones cada vez que hagas el service</h2>
+        <p >
+            Contamos con todo el equipamiento necesario para garantizar el funcionamiento óptimo de tu vehículo, lo que hará que no te preocupes por nada.
+            Nosotros nos ocupamos de cuidar tu auto, vos solamente encargate de
+            disfrutarlo.
+        </p>
+        <img src="" alt="imagen2">
+    </article>
+
+    <hr>
+
+    <!--se me hace que queda medio fulera la tabla pero no se me ocurre otra cosa-->
+    <!--se podria armar dinamicamente?-->
+
+    <table >
+        <tbody>
+        <tr>
+            <th scope="row">10k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">20k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">30k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td>Past.Freno</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">40k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">50k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td>Batería</td>
+            <td>Amortiguadores</td>
+            <td>Neumáticos</td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">60k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td>Past.Freno</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">70k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">80k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">90k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td>Past.Freno</td>
+            <td>Amortiguadores</td>
+            <td>Disc.Freno</td>
+            <td>Neumáticos</td>
+        </tr>
+        <tr>
+            <th scope="row">100k</th>
+            <td>Lámparas</td>
+            <td>Escobillas</td>
+            <td>Batería</td>
+            <td>Embrague</td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/login',function (){
 Route::get('/register',function (){
     return view('auth.register');
 });
+
+Route::get('/services',function (){
+    return view('web.services');
+});
