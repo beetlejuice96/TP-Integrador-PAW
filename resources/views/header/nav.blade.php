21f11fce05b4e093604e716950723ebb81abfe6d
beetlejuice96 <dilanbernbae043@gmail.com>
2021-02-04 12:39:59 -0300

    implmenetando hamburguesa(falta acomodar pero la funcionalidad esta.)

--- resources/views/header/nav.blade.php
+++ resources/views/header/nav.blade.php
@@ -1,10 +1,20 @@
-<ul class="header__menu">
-    <!--estos son LI de ejemplo-->
-    <li><a href="#">part1</a></li>
-    <li><a href="#">part2</a></li>
-    <li><a href="#">part3</a></li>
-    <li><a href="#">part4</a></li>
+<div class="header__div">
 
-</ul>
+    <input class="header__hamburguesa" type="checkbox">
+    <span class="header__hamburguesaLinea"></span>
+    <span class="header__hamburguesaLinea"></span>
+    <span class="header__hamburguesaLinea"></span>
+
+    <ul class="header__menu">
+        <!--estos son LI de ejemplo-->
+        <a class="header__elementoLista" href="#"><li>part1</li></a>
+        <a href="#"><li>part2</li></a>
+        <a href="#"><li>part3</li></a>
+        <a href="#"><li>part4</li></a>
+        <a href="#"><li>part5</li></a>
+
+    </ul>
+</div>
+
 
 
