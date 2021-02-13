72bf9dfeaf9fa1e92fbb9a15b2ada0b7da1930a1
Agustin Normand <NormandAgustin@gmail.com>
2021-02-13 10:58:48 -0300

    Hago que el logo el sitio sea un link al home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('web.home');
-});
+})->name("index");
 
 Route::get('/login',function (){
     return view('auth.login');
