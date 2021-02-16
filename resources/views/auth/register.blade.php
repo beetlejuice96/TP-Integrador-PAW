01c0a9c8a8ac071427040da3c3fa9c915fbe615f
beetlejuice96 <dilanbernbae043@gmail.com>
2021-02-16 09:07:50 -0300

    register arreglado pero sin imagen.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,14 +8,7 @@
 @section('content')
     <form action="" method="POST" class="form-register">
         <section class="card-register">
-            <!--<label for="tipo_dni">Tipo DNI</label>-->
-            <!--da que vaya un select?-->
-            <!--capas jodan con autocompletado-->
-            <select name="tipo_dni" id="tipo_dni"  class="input-login" required autofocus>
-                <!--aca se llena de forma dinamica.-->
-                <option value="value1">DNI</option>
 
-            </select>
 
             <!--<label for="dni">DNI</label>-->
             <input type="dni" id="dni" class="input-login" placeholder="NRO DNI" required autocomplete="dni" autofocus>
@@ -34,6 +27,15 @@
 
              <!--<label for="password-confirm" >Confirmar contraseña</label>-->
              <input id="password-confirm" type="password"  class="input-login" placeholder="Confirmar contraseña" name="password_confirmation" required autocomplete="new-password">
+
+             <!--<label for="tipo_dni">Tipo DNI</label>-->
+             <!--da que vaya un select?-->
+             <!--capas jodan con autocompletado-->
+             <select name="tipo_dni" id="tipo_dni"  class="input-login" required autofocus>
+                <!--aca se llena de forma dinamica.-->
+                <option value="value1">DNI</option>
+
+             </select>
 
              <button type="submit" class="buttonCTA buttonCTA-Primary">Registrar</button>
         </section>
