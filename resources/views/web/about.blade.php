d0d2145906fb39ad2e04dfee679851dd63520ffe
beetlejuice96 <dilanbernbae043@gmail.com>
2021-02-21 18:54:19 -0300

    primer version de about us.

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -2,14 +2,32 @@
 @section('location')
     About
 @endsection
-
+@section('public')
+    <link href="{{ asset('css/about.css') }}" rel="stylesheet">
+@endsection
 @section('content')
-    <h1>¿Quiénes somos?</h1>
-    <p id="description">
-        MechanicSheep se encarga de todo para tu tranquilidad y seguridad. Al confiarnos el mantenimiento y reparación de tu Renault, podés estar seguro de que todo será rápido y fácil: equipos para escucharte y aconsejarte, repuestos Renault originales de alta calidad y soluciones a la medida independientemente de la tarea a realizar o de la antigüedad de tu vehículo.
-    </p>
-    <hr class="">
-    <img src="" alt="imagenMechanic1">
-    <img src="" alt="imagenMechanic2">
-    <img src="" alt="imagenMechanic3">
+
+         <section class="card-about">
+            <h2 class="card-titulo">¿Quiénes somos?</h2>
+            <p id="description">
+                Somos Agentes oficiales Renault hace más de 25 años, capacitados para asegurar el funcionamiento de tu vehículo y evitar posibles pérdidas de garantía.
+            </p>
+         </section>
+          <section  class="card-about">
+              <h2  class="card-titulo">Nuestra vision</h2>
+              <p>
+                  Nos interesa brindar a nuestros clientes toda la tranquilidad y seguridad necesaria a la hora de confiarnos el mantenimiento y reparación de su Renault.
+              </p>
+          </section>
+          <section  class="card-about">
+              <h2  class="card-titulo">Nuestro trabajo</h2>
+              <p>
+                  Trabajamos de manera rápida y eficiente, poniendo a tu disposición todas las facilidades que necesitás. No importa la tarea a realizar o la antigüedad de tu vehículo, nuestro equipo te aconsejará ofreciéndote repuestos originales de la más alta calidad y soluciones a tu medida.
+              </p>
+          </section>
+        <hr class="">
+        <img src="" alt="imagenMechanic1">
+        <img src="" alt="imagenMechanic2">
+        <img src="" alt="imagenMechanic3">
+
 @endsection
